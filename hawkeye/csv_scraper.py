6965ebbe0aa92b4a82b69fced610b8f336a87265
Andreas Arnholm <andreas@arnholm.org>
2021-05-16 15:11:40 +0200

    change csvscraper

--- hawkeye/csv_scraper.py
+++ hawkeye/csv_scraper.py
@@ -5,7 +5,7 @@ import csv
 import numpy as np
 
 def get_data_from_csv(csvdir):
-    csvfiles = glob.glob(os.path.join(csvdir, '*/*.csv'))
+    csvfiles = glob.glob(os.path.join(csvdir, '*.csv'))
     
     csvdicts = {}
 
@@ -24,7 +24,7 @@ def get_data_from_csv(csvdir):
                 if row['Test Information'] == 'TEST STATUS': #Ghetto method of stopping the loop lmao XD
                     key = source_probe + ":" + destination_probe
                     if key in csvdicts:
-                        csvdicts[key][end_execution_time ] = csvfile
+                        csvdicts[key][end_execution_time] = csvfile
                     else:
                         csvdicts[key] = {end_execution_time : csvfile}                    
                     break
@@ -50,14 +50,15 @@ def get_data_from_csv(csvdir):
         
             i += 1
             first_row_passed = False
+            
 
-        command = csvdir.split("/")[0].replace("_", " ")
+        title = csvdir.split("/")[0].replace("_", " ")
 
         results = list(map(int, time_value_dict.values()))
 
         avg = sum(results)/len(results)
 
-        legend = "From " + key.split(":")[0] + " to " + key.split(":")[1] + " " + command + ", Average: " + str(round(avg, 3)) + " ms"
+        legend = "From " + key.split(":")[0] + " to " + key.split(":")[1] + ", Average: " + str(round(avg, 3)) + " ms" + "TITLE" + title 
 
         return_dict[legend] = time_value_dict
 
@@ -68,23 +69,38 @@ def movingaverage(interval, window_size):
     window = np.ones(int(window_size))/float(window_size)
     return np.convolve(interval, window, 'same')
 
-window_size = 15
+window_size = 100
 
-dict = get_data_from_csv("with_commands")
-plt.rcParams["figure.figsize"] = (10,10)
-legends = []
+def plotcsvdir(csvdir):
+    dict_ = get_data_from_csv(csvdir)
+    plt.rcParams["figure.figsize"] = (8,6)
+    legends = []
 
-for d in dict:
-    legends.append(d)
-    plt.xlabel("Time since start [s]")
-    plt.ylabel("Delay [ms]")
-    tuple_x_y = [(k,int(v)) for k, v in sorted(dict[d].items(), key=lambda item: item[0])]
-    x_vals = [x[0] for x in tuple_x_y]
-    y_vals = [x[1] for x in tuple_x_y]
-    y_av = movingaverage(y_vals, window_size)
-    x_vals = x_vals[int(window_size/2):][:-int(window_size/2)]
-    y_av = y_av[int(window_size/2):][:-int(window_size/2)]
-    plt.plot(x_vals, y_av)  
-    plt.legend(legends)
-    
-plt.show()
+    sorted_dict = sorted(dict_)
+
+    title = None
+
+    for d in sorted_dict:
+        legend, title = d.split("TITLE")
+        legends.append(legend)
+        plt.xlabel("Time since start [s]")
+        plt.ylabel("Delay [ms]")
+        tuple_x_y = [(k,int(v)) for k, v in sorted(dict_[d].items(), key=lambda item: item[0])]
+        x_vals = [x[0] for x in tuple_x_y]
+        y_vals = [x[1] for x in tuple_x_y]
+        y_av = movingaverage(y_vals, window_size)
+        x_vals = x_vals[int(window_size/2):][:-int(window_size/2)]
+        y_av = y_av[int(window_size/2):][:-int(window_size/2)]
+        plt.title(title)
+        plt.plot(x_vals, y_av)
+        plt.xlim(0, 3200)
+        plt.ylim(0, 45)
+        plt.legend(legends)
+        
+    plt.savefig(title)
+    plt.show()
+
+plotcsvdir("Lab_no_commands")
+plotcsvdir("Lab_with_commands")
+plotcsvdir("Office_no_commands")
+plotcsvdir("Office_with_commands")
